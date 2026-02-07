ef105b5b0a11be06f72d523ec7b37a48a996f5f8
Maevasabine <106099296+Maevasabine@users.noreply.github.com>
2026-02-07 00:36:45 -0500

    add CORS for vercel

--- campus_driver/settings.py
+++ campus_driver/settings.py
@@ -139,4 +139,7 @@ MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "https://campus-driverfrontend.vercel.app",
+]
+CORS_ALLOW_CREDENTIALS = True
