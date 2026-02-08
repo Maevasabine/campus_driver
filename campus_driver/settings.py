a96cbb3aa18ef2ffc07249328c6521b72cd14b02
Maevasabine <106099296+Maevasabine@users.noreply.github.com>
2026-02-08 18:24:09 -0500

    fix database config

--- campus_driver/settings.py
+++ campus_driver/settings.py
@@ -88,11 +88,7 @@ WSGI_APPLICATION = "campus_driver.wsgi.application"
 
 
 DATABASES = {
-    "default": dj_database_url.config(
-        default=os.environ.get("DATABASE_URL"),
-        conn_max_age=600,
-        ssl_require=True
-    )
+    "default": dj_database_url.config(conn_max_age=600)
 }
 
 
