479d6d9e08be7ed6ed9abbe499b98d8a200156fa
Maevasabine <106099296+Maevasabine@users.noreply.github.com>
2026-02-08 21:38:43 -0500

    fix database config

--- campus_driver/settings.py
+++ campus_driver/settings.py
@@ -86,12 +86,22 @@ WSGI_APPLICATION = "campus_driver.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    "default": dj_database_url.parse(
-        os.environ.get("DATABASE_PUBLIC_URL"),
-        conn_max_age=600,
-    )
-}
+DATABASE_URL = os.environ.get("DATABASE_PUBLIC_URL")
+if DATABASE_URL:
+    print("DATABASE_URL =", repr(os.environ.get("DATABASE_PUBLIC_URL")))
+    DATABASES = {
+        "default": dj_database_url.parse(
+            DATABASE_URL,
+            conn_max_age=600,
+        )
+    }
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 
 
 
